c88e8d4b681ed499562241ae2e34f675d5d48a68
Fernan A. Cabrera <fernan.cabrera.email@gmail.com>
2021-07-21 22:06:32 +0800

    Duplicate Files

--- prjVipTcpIPcontroler/Forms/frmReportLogs.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class frmReportLogs
-
-End Class
--- prjVipTcpIPcontroler/Modules/Network_Function.vb
+++
@@ -1,9 +0,0 @@
-﻿Module Network_Function
-    Public Function PingMe(ByVal IPaddress As String) As Boolean
-        If My.Computer.Network.Ping(IPaddress, 5) = False Then
-            Return False
-        Else
-            Return True
-        End If
-    End Function
-End Module
